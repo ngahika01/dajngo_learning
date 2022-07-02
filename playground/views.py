8aead68103e3e868d3c0e0fd41b8c1991047c0a9
nghaika01 <mykenjoroge01@gmail.com>
2022-07-02 12:21:06 +0300

    debugging

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+
 def say_hello(request):
-    return render(request, 'hello.html',{'name':"Mike"})
+    x = calculate()
+    y = 2
+    return render(request, 'hello.html', {'name': "Mike"})
