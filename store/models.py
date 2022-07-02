e964b4e16eb41dc62f5cdbaa07350e977d0dd9fe
nghaika01 <mykenjoroge01@gmail.com>
2022-07-02 13:04:04 +0300

    Added product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+
+class Product():
+    title = models.CharField(max_length=255)
+    description = models.TimeField()
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    inventory = models.IntegerField()
+    last_update = models.DateField(auto_now=True)
+    
