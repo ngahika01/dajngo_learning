a80c2034beb45945cf057efb54624ece13f5a530
nghaika01 <mykenjoroge01@gmail.com>
2022-07-02 14:16:06 +0300

    Added generic filed for likes and tags

--- store/models.py
+++ store/models.py
@@ -7,6 +7,8 @@ from django.db import models
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Promotion(models.Model):
