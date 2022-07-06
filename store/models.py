4a0671779333283fc556fb3d5622c2f1b7c8bc26
nghaika01 <mykenjoroge01@gmail.com>
2022-07-06 04:14:45 +0300

    starting on orms

--- store/models.py
+++ store/models.py
@@ -1,8 +1,9 @@
-import email
-from itertools import product
-from pyexpat import model
 from django.db import models
-from django.db import models
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
 
 
 class Collection(models.Model):
@@ -11,27 +12,22 @@ class Collection(models.Model):
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
-class Promotion(models.Model):
-    description = models.CharField(max_length=255)
-    discount = models.FloatField()
-    # product_name
-
-
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TimeField()
-    unit_price = models.DecimalField(decimal_places=2, max_digits=6)
-    inventory = models.IntegerField()
-    last_update = models.DateField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.PROTECT,)
-    promotions = models.ManyToManyField(Promotion)
     slug = models.SlugField()
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
+
     MEMBERSHIP_CHOICES = [
         (MEMBERSHIP_BRONZE, 'Bronze'),
         (MEMBERSHIP_SILVER, 'Silver'),
@@ -45,24 +41,25 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+
 class Order(models.Model):
-    PAYMENT_PENDING = 'P'
-    PAYMENT_COMPLETE = 'C'
-    PAYMENT_FAILED = 'F'
-    PAYMENT_CHOICES = [
-        (PAYMENT_PENDING, 'Pending'),
-        (PAYMENT_COMPLETE, 'Complete'),
-        (PAYMENT_FAILED, 'Failed')
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
     ]
 
-    placed_at = models.DateField(auto_now_add=True)
+    placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
-        max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT),
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -73,7 +70,6 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE)
-    zip = models.CharField(max_length=255)
 
 
 class Cart(models.Model):
